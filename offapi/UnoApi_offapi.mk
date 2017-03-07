f97bd0c422952fd258eeee2434b930d23239ec50
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2017-03-07 16:34:14 +0100

    Pivot chart specific data provider to get pivot table output data
    Change-Id: I6e1c4320316e7dcfad5b1de5d55403cfebd01a29

--- offapi/UnoApi_offapi.mk
+++ offapi/UnoApi_offapi.mk
@@ -2065,6 +2065,7 @@ $(eval $(call gb_UnoApi_add_idlfiles,offapi,com/sun/star/chart2/data,\
 	XLabeledDataSequence \
 	XLabeledDataSequence2 \
 	XNumericalDataSequence \
+	XPivotChartDataProvider \
 	XPopupRequest \
 	XRangeHighlighter \
 	XRangeXMLConversion \
